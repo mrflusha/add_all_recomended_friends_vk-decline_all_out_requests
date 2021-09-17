519518a1ca3f215708c9428489faea4c6d84f5c1
Sergey <46491304+mrflusha@users.noreply.github.com>
2021-09-17 20:58:48 +0300

    timeouts upd

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-decline_friend = document.querySelectorAll("[id^= friends_deny_request_");
-for (var i = 0; i <= decline_friend.length; i++){
-    decline_friend[i].click()
+var timer = 10000
+var decline_friend = document.querySelectorAll("[id^= friends_deny_request_");
+
+(async() => {for (var i = 0; i <= decline_friend.length; i++){
+    decline_friend[i].click();
+    await delay();
+} console.log("helloworld");})()
+
+
+function delay() {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, timer);
+    });
 }
